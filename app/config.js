1f6f595d5932b919ae4068c1e7e807fb2204ba29
tiagogt0000 <tiagogaetanotlili@gmail.com>
2026-09-26 19:14:16 +0200

    Bump app version for feedback flow

--- app/config.js
+++ app/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = Object.freeze({
-  version: '1.0.0',
+  version: '1.1.0',
   app: 'english-practice',
   webAppUrl: 'https://script.google.com/macros/s/AKfycbyR6SVBiVahbc94gOTIpL4Iod9Fsogu8EYj5Il5l4V7pQwQ1UaXudsdV0Sgad9ZsTdz/exec',
   timeZone: 'Europe/Berlin'
